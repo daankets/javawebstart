2c511e52b31961161d9248977d534e057ad0c5d5
Daan Kets <daankets@deltasource.eu>
2021-06-03 21:31:58 +0200

    Refactored tests

--- test/java-web-start.spec.ts
+++ test/java-web-start.spec.ts
@@ -6,45 +6,39 @@ import path from "path";
 import {parseXml} from "libxmljs2";
 import {URL} from "url";
 
+const JNLP_URL = "https://raw.githubusercontent.com/daankets/javawebstart/develop/test/sample.jnlp";
+const JAR_URL =  "https://raw.githubusercontent.com/daankets/javawebstart/develop/test/sample.jar";
+const EXPECTED_META_INFO = Object.freeze({
+	vendor: "Daan Kets",
+	title: "Sample JNLP",
+	homepage: "https://github.com/daankets/javawebstart",
+	jarLocation: JAR_URL,
+	mainClass: "sample.MainClass"
+});
+
 describe("The JavaWebStart package", () => {
 	context("Parsing", () => {
 		it("Can parse a JNLP file from a string", () => {
-			const testData = readFileSync(path.join(__dirname, "sample-jnlp.xml"), {encoding: "utf-8"});
+			const testData = readFileSync(path.join(__dirname, "sample.jnlp"), {encoding: "utf-8"});
 			const jws = JavaWebStart.parse(testData);
-			assert.deepEqual(jws.meta, {
-				vendor: "Daan Kets",
-				title: "Sample JNLP",
-				homepage: "http://host.domain",
-				jarLocation: "http://host.domain/path/sample.jar",
-				mainClass: "sample.MainClass"
-			}, "Invalid meta info");
+			assert.deepEqual(jws.meta, EXPECTED_META_INFO, "Invalid meta info");
 		});
 		it("Can be constructed from an XML document", () => {
-			const testData = readFileSync(path.join(__dirname, "sample-jnlp.xml"), {encoding: "utf-8"});
+			const testData = readFileSync(path.join(__dirname, "sample.jnlp"), {encoding: "utf-8"});
 			const xml = parseXml(testData);
 			const jws = new JavaWebStart(xml);
-			assert.deepEqual(jws.meta, {
-				vendor: "Daan Kets",
-				title: "Sample JNLP",
-				homepage: "http://host.domain",
-				jarLocation: "http://host.domain/path/sample.jar",
-				mainClass: "sample.MainClass"
-			}, "Invalid meta info");
+			assert.deepEqual(jws.meta, EXPECTED_META_INFO, "Invalid meta info");
 		});
-		it("Can load a file", async() => {
-			const jws = await JavaWebStart.downloadJNLP(new URL("file://" + path.join(__dirname, "sample-jnlp.xml")));
-			assert.deepEqual(jws.meta, {
-				vendor: "Daan Kets",
-				title: "Sample JNLP",
-				homepage: "http://host.domain",
-				jarLocation: "http://host.domain/path/sample.jar",
-				mainClass: "sample.MainClass"
-			}, "Invalid meta info");
+		it("Can load a local file", async() => {
+			const jws = await JavaWebStart.downloadJNLP(new URL("file://" + path.join(__dirname, "sample.jnlp")));
+			assert.deepEqual(jws.meta, EXPECTED_META_INFO, "Invalid meta info");
 		});
 	});
 	context("Downloading", () => {
-		it("Can download a hosted JNLP file", () => {
-
+		it("Can download a hosted JNLP file", async () => {
+			const jws = await JavaWebStart.downloadJNLP(new URL(JNLP_URL));
+			assert.deepEqual(jws.meta, EXPECTED_META_INFO, "Invalid meta info");
 		});
 	});
+	context("Running")
 });
